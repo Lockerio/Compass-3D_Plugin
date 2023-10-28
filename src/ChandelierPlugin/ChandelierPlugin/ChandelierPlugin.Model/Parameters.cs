87d9d1d4a72a457157d46afd7742a392c0d85d51
Arkadiy <goryunov.arkadiy@gmail.com>
2023-10-28 20:23:41 +0700

    Add Parameters.cs

---
+++ src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Parameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChandelierPlugin.Model
+{
+    public class Parameters
+    {
+        private double _radiusOuterCircle;
+        private double _radiusInnerCircle;
+        private double _radiusBaseCircle;
+        private double _foundationThickness;
+        private int _lampsAmount;
+        private double _lampRadius;
+    }
+}
