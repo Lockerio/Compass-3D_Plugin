00fece2f10bbe9330529072688ac0ea97e8f3f9f
Arkadiy <goryunov.arkadiy@gmail.com>
2023-10-28 18:53:12 +0700

    Add function IsNumberInRange

--- src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Validator.cs
+++ src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Validator.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace ChandelierPlugin.Model
 {
-    internal class Validator
+    public class Validator
     {
+        private bool IsNumberInRange(double value, double min, double max) 
+        {
+            if (min < value && value < max)
+                return true;
+            return false;
+        }
 
+        public void AssertNumberIsInRange(double value, double min, double max)
+        {
+            if (IsNumberInRange(value, min, max))
+                return;
+            throw new Exception("Ваше число не попадает в диапазон доступных чисел!");
+        }
     }
 }
