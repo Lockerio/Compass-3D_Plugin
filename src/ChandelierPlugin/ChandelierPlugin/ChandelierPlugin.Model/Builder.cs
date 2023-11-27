23f2ed7bdd3eee8a6a2a71a9757a29d6d0e3e78d
Arkadiy <goryunov.arkadiy@gmail.com>
2023-11-27 17:15:09 +0700

    Refactor Model

--- src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Builder.cs
+++ src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Builder.cs
@@ -57,7 +57,7 @@ namespace ChandelierPlugin.Model
 
         private void BuildBase(double radiusInnerCircle, double radiusOuterCircle, double radiusBaseCircle)
         {
-            var sketch = this.CreateSketch(Obj3dType.o3d_planeXOY, null);
+            var sketch = _wrapper.CreateSketch(Obj3dType.o3d_planeXOY, null);
             var document2d = (ksDocument2D)sketch.BeginEdit();
 
             document2d.ksCircle(0, 0, radiusBaseCircle, 1);
@@ -65,103 +65,36 @@ namespace ChandelierPlugin.Model
             document2d.ksCircle(0, 0, radiusOuterCircle, 1);
             sketch.EndEdit();
 
-            this.СreateExtrusion(sketch, this.foundationThickness, true);
+            _wrapper.СreateExtrusion(sketch, this.foundationThickness, true);
         }
 
         private void BuildWiresTubes(double radius)
         {
-            var offsetWidthEntity = this.CreateOffsetPlane(Obj3dType.o3d_planeXOZ, this.radiusBaseCircle);
-            var sketch = this.CreateSketch(Obj3dType.o3d_planeXOZ, offsetWidthEntity);
+            var offsetWidthEntity = _wrapper.CreateOffsetPlane(Obj3dType.o3d_planeXOZ, this.radiusBaseCircle);
+            var sketch = _wrapper.CreateSketch(Obj3dType.o3d_planeXOZ, offsetWidthEntity);
             var document2d = (ksDocument2D)sketch.BeginEdit();
 
             document2d.ksCircle(0, -this.foundationThickness / 2, radius, 1);
             sketch.EndEdit();
 
-            var extrusionDef = this.СreateExtrusionToNearSurface(sketch, false);
-            this.CreateCircularCopy(3, extrusionDef);
+            var extrusionDef = _wrapper.СreateExtrusionToNearSurface(sketch, false);
+            _wrapper.CreateCircularCopy(3, extrusionDef);
 
-            extrusionDef = this.СreateExtrusionToNearSurface(sketch, true);
-            this.CreateCircularCopy(3, extrusionDef);
+            extrusionDef = _wrapper.СreateExtrusionToNearSurface(sketch, true);
+            _wrapper.CreateCircularCopy(3, extrusionDef);
         }
 
         private void BuildLamps(int lampsAmount, double lampRadius)
         {
-            var sketch = this.CreateSketch(Obj3dType.o3d_planeXOY, null);
+            var sketch = _wrapper.CreateSketch(Obj3dType.o3d_planeXOY, null);
             var document2d = (ksDocument2D)sketch.BeginEdit();
 
             var offset = (this.radiusOuterCircle + this.radiusInnerCircle) / 2;
             document2d.ksCircle(offset, 0, lampRadius, 1);
             sketch.EndEdit();
 
-            var extrusionDef = this.СreateExtrusion(sketch, 20, false);
-            this.CreateCircularCopy(lampsAmount, extrusionDef);
-        }
-
-        private ksEntity CreateOffsetPlane(Obj3dType plane, double offset)
-        {
-            var offsetEntity = (ksEntity)this._part.NewEntity((short)Obj3dType.o3d_planeOffset);
-            var offsetDef = (ksPlaneOffsetDefinition)offsetEntity.GetDefinition();
-            offsetDef.SetPlane((ksEntity)this._part.NewEntity((short)plane));
-            offsetDef.offset = offset;
-            offsetDef.direction = false;
-            offsetEntity.Create();
-            return offsetEntity;
-        }
-
-        private ksSketchDefinition CreateSketch(Obj3dType planeType, ksEntity offsetPlane)
-        {
-            var plane = (ksEntity)this._part.GetDefaultEntity((short)planeType);
-            var sketch = (ksEntity)this._part.NewEntity((short)Obj3dType.o3d_sketch);
-            var ksSketch = (ksSketchDefinition)sketch.GetDefinition();
-
-            if (offsetPlane != null)
-            {
-                ksSketch.SetPlane(offsetPlane);
-                sketch.Create();
-                return ksSketch;
-            }
-
-            ksSketch.SetPlane(plane);
-            sketch.Create();
-            return ksSketch;
-        }
-
-        private ksBossExtrusionDefinition СreateExtrusion(ksSketchDefinition sketch, double depth, bool side = true)
-        {
-            var extrusionEntity = (ksEntity)this._part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
-            var extrusionDef = (ksBossExtrusionDefinition)extrusionEntity.GetDefinition();
-
-            extrusionDef.SetSideParam(side, (short)End_Type.etBlind, depth);
-            extrusionDef.directionType = side ? (short)Direction_Type.dtNormal : (short)Direction_Type.dtReverse;
-            extrusionDef.SetSketch(sketch);
-            extrusionEntity.Create();
-
-            return extrusionDef;
-        }
-
-        private ksBossExtrusionDefinition СreateExtrusionToNearSurface(ksSketchDefinition sketch, bool side = true)
-        {
-            var extrusionEntity = (ksEntity)this._part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
-            var extrusionDef = (ksBossExtrusionDefinition)extrusionEntity.GetDefinition();
-
-            extrusionDef.SetSideParam(side, (short)End_Type.etUpToNearSurface);
-            extrusionDef.directionType = side ? (short)Direction_Type.dtNormal : (short)Direction_Type.dtReverse;
-            extrusionDef.SetSketch(sketch);
-            extrusionEntity.Create();
-
-            return extrusionDef;
-        }
-
-        private void CreateCircularCopy(int count, object definition)
-        {
-            ksEntity entity = this._part.NewEntity((short)Obj3dType.o3d_circularCopy);
-            ksCircularCopyDefinition copyDefinition = entity.GetDefinition();
-            copyDefinition.SetCopyParamAlongDir(count, 360, true, false);
-            ksEntity baseAxisOz = this._part.GetDefaultEntity((short)Obj3dType.o3d_axisOZ);
-            copyDefinition.SetAxis(baseAxisOz);
-            ksEntityCollection entityCollection = copyDefinition.GetOperationArray();
-            entityCollection.Add(definition);
-            entity.Create();
+            var extrusionDef = _wrapper.СreateExtrusion(sketch, 20, false);
+            _wrapper.CreateCircularCopy(lampsAmount, extrusionDef);
         }
     }
 }
--- src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Wrapper.cs
+++ src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Wrapper.cs
@@ -54,5 +54,72 @@ namespace ChandelierPlugin.Model
             _part = document3D.GetPart((int)Part_Type.pTop_Part);
             return document3D;
         }
+
+        public ksEntity CreateOffsetPlane(Obj3dType plane, double offset)
+        {
+            var offsetEntity = (ksEntity)this._part.NewEntity((short)Obj3dType.o3d_planeOffset);
+            var offsetDef = (ksPlaneOffsetDefinition)offsetEntity.GetDefinition();
+            offsetDef.SetPlane((ksEntity)this._part.NewEntity((short)plane));
+            offsetDef.offset = offset;
+            offsetDef.direction = false;
+            offsetEntity.Create();
+            return offsetEntity;
+        }
+
+        public ksSketchDefinition CreateSketch(Obj3dType planeType, ksEntity offsetPlane)
+        {
+            var plane = (ksEntity)this._part.GetDefaultEntity((short)planeType);
+            var sketch = (ksEntity)this._part.NewEntity((short)Obj3dType.o3d_sketch);
+            var ksSketch = (ksSketchDefinition)sketch.GetDefinition();
+
+            if (offsetPlane != null)
+            {
+                ksSketch.SetPlane(offsetPlane);
+                sketch.Create();
+                return ksSketch;
+            }
+
+            ksSketch.SetPlane(plane);
+            sketch.Create();
+            return ksSketch;
+        }
+
+        public ksBossExtrusionDefinition СreateExtrusion(ksSketchDefinition sketch, double depth, bool side = true)
+        {
+            var extrusionEntity = (ksEntity)this._part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
+            var extrusionDef = (ksBossExtrusionDefinition)extrusionEntity.GetDefinition();
+
+            extrusionDef.SetSideParam(side, (short)End_Type.etBlind, depth);
+            extrusionDef.directionType = side ? (short)Direction_Type.dtNormal : (short)Direction_Type.dtReverse;
+            extrusionDef.SetSketch(sketch);
+            extrusionEntity.Create();
+
+            return extrusionDef;
+        }
+
+        public ksBossExtrusionDefinition СreateExtrusionToNearSurface(ksSketchDefinition sketch, bool side = true)
+        {
+            var extrusionEntity = (ksEntity)this._part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
+            var extrusionDef = (ksBossExtrusionDefinition)extrusionEntity.GetDefinition();
+
+            extrusionDef.SetSideParam(side, (short)End_Type.etUpToNearSurface);
+            extrusionDef.directionType = side ? (short)Direction_Type.dtNormal : (short)Direction_Type.dtReverse;
+            extrusionDef.SetSketch(sketch);
+            extrusionEntity.Create();
+
+            return extrusionDef;
+        }
+
+        public void CreateCircularCopy(int count, object definition)
+        {
+            ksEntity entity = this._part.NewEntity((short)Obj3dType.o3d_circularCopy);
+            ksCircularCopyDefinition copyDefinition = entity.GetDefinition();
+            copyDefinition.SetCopyParamAlongDir(count, 360, true, false);
+            ksEntity baseAxisOz = this._part.GetDefaultEntity((short)Obj3dType.o3d_axisOZ);
+            copyDefinition.SetAxis(baseAxisOz);
+            ksEntityCollection entityCollection = copyDefinition.GetOperationArray();
+            entityCollection.Add(definition);
+            entity.Create();
+        }
     }
 }
