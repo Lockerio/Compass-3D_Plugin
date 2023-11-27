b9b518631fbb7e3d7786ace034fb64636d0cda2e
Arkadiy <goryunov.arkadiy@gmail.com>
2023-11-27 17:11:49 +0700

    Update Builder

--- src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Builder.cs
+++ src/ChandelierPlugin/ChandelierPlugin/ChandelierPlugin.Model/Builder.cs
@@ -35,24 +35,24 @@ namespace ChandelierPlugin.Model
                 return;
             }
 
-            _kompas = _wrapper.Kompas;
-            _doc3D = _wrapper.CreateDocument3D();
-            _doc3D.Create();
-            _part = _wrapper.Part;
+            this._kompas = this._wrapper.Kompas;
+            this._doc3D = this._wrapper.CreateDocument3D();
+            this._doc3D.Create();
+            this._part = this._wrapper.Part;
         }
 
         public void BuildDetail(Parameters parameters)
         {
-            radiusInnerCircle = parameters.ParametersDict[ParameterType.RadiusInnerCircle].CurrentValue;
-            radiusOuterCircle = parameters.ParametersDict[ParameterType.RadiusOuterCircle].CurrentValue;
-            radiusBaseCircle = parameters.ParametersDict[ParameterType.RadiusBaseCircle].CurrentValue;
-            foundationThickness = parameters.ParametersDict[ParameterType.FoundationThickness].CurrentValue;
-            lampsAmount = (int)parameters.ParametersDict[ParameterType.LampsAmount].CurrentValue;
-            lampRadius = parameters.ParametersDict[ParameterType.LampRadius].CurrentValue;
+            this.radiusInnerCircle = parameters.ParametersDict[ParameterType.RadiusInnerCircle].CurrentValue;
+            this.radiusOuterCircle = parameters.ParametersDict[ParameterType.RadiusOuterCircle].CurrentValue;
+            this.radiusBaseCircle = parameters.ParametersDict[ParameterType.RadiusBaseCircle].CurrentValue;
+            this.foundationThickness = parameters.ParametersDict[ParameterType.FoundationThickness].CurrentValue;
+            this.lampsAmount = (int)parameters.ParametersDict[ParameterType.LampsAmount].CurrentValue;
+            this.lampRadius = parameters.ParametersDict[ParameterType.LampRadius].CurrentValue;
 
-            BuildBase(radiusInnerCircle, radiusOuterCircle, radiusBaseCircle);
-            BuildWiresTubes(15);
-            BuildLamps(lampsAmount, lampRadius);
+            this.BuildBase(this.radiusInnerCircle, this.radiusOuterCircle, this.radiusBaseCircle);
+            this.BuildWiresTubes(15);
+            this.BuildLamps(this.lampsAmount, this.lampRadius);
         }
 
         private void BuildBase(double radiusInnerCircle, double radiusOuterCircle, double radiusBaseCircle)
@@ -78,10 +78,10 @@ namespace ChandelierPlugin.Model
             sketch.EndEdit();
 
             var extrusionDef = this.СreateExtrusionToNearSurface(sketch, false);
-            CreateCircularCopy(3, extrusionDef);
+            this.CreateCircularCopy(3, extrusionDef);
 
             extrusionDef = this.СreateExtrusionToNearSurface(sketch, true);
-            CreateCircularCopy(3, extrusionDef);
+            this.CreateCircularCopy(3, extrusionDef);
         }
 
         private void BuildLamps(int lampsAmount, double lampRadius)
@@ -94,14 +94,14 @@ namespace ChandelierPlugin.Model
             sketch.EndEdit();
 
             var extrusionDef = this.СreateExtrusion(sketch, 20, false);
-            CreateCircularCopy(lampsAmount, extrusionDef);
+            this.CreateCircularCopy(lampsAmount, extrusionDef);
         }
 
         private ksEntity CreateOffsetPlane(Obj3dType plane, double offset)
         {
-            var offsetEntity = (ksEntity)_part.NewEntity((short)Obj3dType.o3d_planeOffset);
+            var offsetEntity = (ksEntity)this._part.NewEntity((short)Obj3dType.o3d_planeOffset);
             var offsetDef = (ksPlaneOffsetDefinition)offsetEntity.GetDefinition();
-            offsetDef.SetPlane((ksEntity)_part.NewEntity((short)plane));
+            offsetDef.SetPlane((ksEntity)this._part.NewEntity((short)plane));
             offsetDef.offset = offset;
             offsetDef.direction = false;
             offsetEntity.Create();
@@ -110,8 +110,8 @@ namespace ChandelierPlugin.Model
 
         private ksSketchDefinition CreateSketch(Obj3dType planeType, ksEntity offsetPlane)
         {
-            var plane = (ksEntity)_part.GetDefaultEntity((short)planeType);
-            var sketch = (ksEntity)_part.NewEntity((short)Obj3dType.o3d_sketch);
+            var plane = (ksEntity)this._part.GetDefaultEntity((short)planeType);
+            var sketch = (ksEntity)this._part.NewEntity((short)Obj3dType.o3d_sketch);
             var ksSketch = (ksSketchDefinition)sketch.GetDefinition();
 
             if (offsetPlane != null)
@@ -128,7 +128,7 @@ namespace ChandelierPlugin.Model
 
         private ksBossExtrusionDefinition СreateExtrusion(ksSketchDefinition sketch, double depth, bool side = true)
         {
-            var extrusionEntity = (ksEntity)_part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
+            var extrusionEntity = (ksEntity)this._part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
             var extrusionDef = (ksBossExtrusionDefinition)extrusionEntity.GetDefinition();
 
             extrusionDef.SetSideParam(side, (short)End_Type.etBlind, depth);
@@ -141,7 +141,7 @@ namespace ChandelierPlugin.Model
 
         private ksBossExtrusionDefinition СreateExtrusionToNearSurface(ksSketchDefinition sketch, bool side = true)
         {
-            var extrusionEntity = (ksEntity)_part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
+            var extrusionEntity = (ksEntity)this._part.NewEntity((short)ksObj3dTypeEnum.o3d_bossExtrusion);
             var extrusionDef = (ksBossExtrusionDefinition)extrusionEntity.GetDefinition();
 
             extrusionDef.SetSideParam(side, (short)End_Type.etUpToNearSurface);
@@ -154,10 +154,10 @@ namespace ChandelierPlugin.Model
 
         private void CreateCircularCopy(int count, object definition)
         {
-            ksEntity entity = _part.NewEntity((short)Obj3dType.o3d_circularCopy);
+            ksEntity entity = this._part.NewEntity((short)Obj3dType.o3d_circularCopy);
             ksCircularCopyDefinition copyDefinition = entity.GetDefinition();
             copyDefinition.SetCopyParamAlongDir(count, 360, true, false);
-            ksEntity baseAxisOz = _part.GetDefaultEntity((short)Obj3dType.o3d_axisOZ);
+            ksEntity baseAxisOz = this._part.GetDefaultEntity((short)Obj3dType.o3d_axisOZ);
             copyDefinition.SetAxis(baseAxisOz);
             ksEntityCollection entityCollection = copyDefinition.GetOperationArray();
             entityCollection.Add(definition);
